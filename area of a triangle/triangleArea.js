63f9adbd10b8cf21bc149027d802cefb3d3f6399
snir2912 <89028623+snir2912@users.noreply.github.com>
2021-11-20 15:50:19 +0200

    haron formula

--- area of a triangle/triangleArea.js
+++ area of a triangle/triangleArea.js
@@ -1,8 +1,10 @@
 let btn = document.getElementById('btn').addEventListener('click', function() {
-    let height = document.getElementById('h').value;
-    let long = document.getElementById('l').value;
-    let basis = document.getElementById('b').value;
-    let s = height + basis + long / 2;
-    let area = Math.sqrt(s * ((s - height) * (s - long) * (s - basis)))
+    let height = parseInt(document.getElementById('h').value);
+    let long = parseInt(document.getElementById('l').value);
+    let basis = parseInt(document.getElementById('b').value);
+    let s = (height + basis + long) / 2;
+    console.log(s);
+    let area = Math.sqrt(s * ((s - height) * (s - long) * (s - basis)));
+    console.log(area);
     document.getElementById('text').innerText = area;
 });
