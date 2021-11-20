5eee4ade6b8650897de2701da2d864b89cd90669
snir2912 <89028623+snir2912@users.noreply.github.com>
2021-11-20 15:52:23 +0200

    delete tasting

--- area of a triangle/triangleArea.js
+++ area of a triangle/triangleArea.js
@@ -3,8 +3,6 @@ let btn = document.getElementById('btn').addEventListener('click', function() {
     let long = parseInt(document.getElementById('l').value);
     let basis = parseInt(document.getElementById('b').value);
     let s = (height + basis + long) / 2;
-    console.log(s);
     let area = Math.sqrt(s * ((s - height) * (s - long) * (s - basis)));
-    console.log(area);
     document.getElementById('text').innerText = area;
 });
