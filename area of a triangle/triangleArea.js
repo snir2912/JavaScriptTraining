e441160ba744d2318207b8ecf6692cb9b3070a52
snir2912 <89028623+snir2912@users.noreply.github.com>
2021-11-20 15:40:14 +0200

    changes

--- area of a triangle/triangleArea.js
+++ area of a triangle/triangleArea.js
@@ -2,6 +2,7 @@ let btn = document.getElementById('btn').addEventListener('click', function() {
     let height = document.getElementById('h').value;
     let long = document.getElementById('l').value;
     let basis = document.getElementById('b').value;
-    let area = height * basis / 2
+    let s = height + basis + long / 2
+    let area = Math.sqrt(s * (s - height)(s - long)(s - basis))
     document.getElementById('text').innerText = area;
 });
